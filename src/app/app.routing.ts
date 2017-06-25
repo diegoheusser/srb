d1213677446fa5e460aa61d4cca07d091a60fbfb
Diego Heusser <diego.heusser@outlook.com>
2017-06-24 23:27:52 -0300

    Criado a rota para o relatório de rebanhos

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,13 +6,15 @@ import { HomeComponent } from './home/home.component';
 import { ScreenUnderConstructionComponent } from './screen-under-construction/screen-under-construction.component';
 import { FormInseminadorComponent } from './form-inseminador/form-inseminador.component';
 import { FormRebanhoComponent } from './form-rebanho/form-rebanho.component';
-import { ReportInseminacaoComponent } from './report-inseminacao/report-inseminacao.component';;
+import { ReportInseminacaoComponent } from './report-inseminacao/report-inseminacao.component';
+import { ReportRebanhoComponent } from './report-rebanho/report-rebanho.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'screen-under-construction', component: ScreenUnderConstructionComponent },
     { path: 'inseminador/novo', component: FormInseminadorComponent },
+    { path: 'rebanho', component: ReportRebanhoComponent },
     { path: 'rebanho/novo', component: FormRebanhoComponent },
     { path: 'inseminacao', component: ReportInseminacaoComponent }
 ];
