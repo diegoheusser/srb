382f5e0da64b83ac26e2eef42fa1b4b588b9ac89
Diego Heusser <diego.heusser@outlook.com>
2017-06-24 23:20:56 -0300

    Criado rota para o cadastro de rebanhos

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,12 +5,14 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ScreenUnderConstructionComponent } from './screen-under-construction/screen-under-construction.component';
 import { FormInseminadorComponent } from './form-inseminador/form-inseminador.component';
+import { FormRebanhoComponent } from './form-rebanho/form-rebanho.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'screen-under-construction', component: ScreenUnderConstructionComponent },
-    { path: 'inseminador/novo', component: FormInseminadorComponent }
+    { path: 'inseminador/novo', component: FormInseminadorComponent },
+    { path: 'rebanho/novo', component: FormRebanhoComponent }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
