cc4fd6f6bf7ac3845d849b0dc12eb5ad95d3736b
Diego Heusser <diego.heusser@outlook.com>
2017-06-24 22:05:19 -0300

    Layout dos menus raiz  concluído

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  optionRegisterIsSelect: boolean;
+  optionReportIsSelect: boolean;
+
   constructor() { }
 
   ngOnInit() {
