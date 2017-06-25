4a635b13b432e517d1637aee17037f7e2faa17b2
Diego Heusser <diego.heusser@outlook.com>
2017-06-24 23:17:18 -0300

    Criado rota para o cadastro de inseminadores

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ScreenUnderConstructionComponent } from './screen-under-construction/screen-under-construction.component';
+import { FormInseminadorComponent } from './form-inseminador/form-inseminador.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'screen-under-construction', component: ScreenUnderConstructionComponent }
+    { path: 'screen-under-construction', component: ScreenUnderConstructionComponent },
+    { path: 'inseminador/novo', component: FormInseminadorComponent }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
